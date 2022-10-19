ddd0511480ecf1b372eb984d744103c03c00b779
mukeshnrjn <98488292+mukeshnrjn@users.noreply.github.com>
2022-10-19 15:13:14 +0530

    initial commit

---
+++ build/precache-manifest.07e57363bdc3e45ec9ac3b35b82a8c0a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "4ebace33eb4f3179c85e",
+    "url": "/my-react-app/static/css/main.76c7e3d7.chunk.css"
+  },
+  {
+    "revision": "4ebace33eb4f3179c85e",
+    "url": "/my-react-app/static/js/main.4ebace33.chunk.js"
+  },
+  {
+    "revision": "95ea83b6c7492a61ef7b",
+    "url": "/my-react-app/static/js/1.95ea83b6.chunk.js"
+  },
+  {
+    "revision": "5cc09886632c805e530e",
+    "url": "/my-react-app/static/js/2.5cc09886.chunk.js"
+  },
+  {
+    "revision": "443bf954d3a8e17a11ae",
+    "url": "/my-react-app/static/js/runtime~main.443bf954.js"
+  },
+  {
+    "revision": "4b43a7c2c5c485ebf8c13dcd118afa49",
+    "url": "/my-react-app/index.html"
+  }
+];
